2587827018c3ca29660ca532dbef92a18df3a1a2
Estevan Bartmann Silveira <estevanbartmann@gmail.com>
2020-05-23 11:20:31 -0300

    Primeira versão

---
+++ jogo.js
@@ -0,0 +1,15 @@
+let pergunta1 = "1 + 1"
+let resposta1 = 2
+console.log(pergunta1)
+
+respostauser(2)
+
+function respostauser(resposta) {
+    if (resposta == resposta1) {
+        console.log("Correto!")
+    }
+    else {
+        console.log("Incorreto")
+    }
+     
+}
